f9fc964b068380fa2e3407d799b81944d63e0249
Abdillah Bakari Ali <abdibaker1@gmail.com>
2023-12-24 09:56:22 +0200

    Add Helper.php file

--- src/App/App.php
+++ src/App/App.php
@@ -11,6 +11,7 @@ $customErrorHandler = require __DIR__ . '/ErrorHandler.php';
 (require __DIR__ . '/Middlewares.php')($app, $customErrorHandler);
 (require __DIR__ . '/Cors.php')($app);
 (require __DIR__ . '/Database.php');
+(require __DIR__ . '/Helper.php');
 (require __DIR__ . '/Services.php');
 (require __DIR__ . '/Routes.php')($app);
 (require __DIR__ . '/NotFound.php')($app);
