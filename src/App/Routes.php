8eabc61f6cc3581626618e1eaec1a7350cc2fd57
Abdillah Bakari <abdibaker1@gmail.com>
2024-06-08 21:16:41 +0300

    refactor: Update file paths in App.php and remove unused files

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -2,13 +2,14 @@
 
 declare(strict_types=1);
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
+use Slim\App;
 
-return static function ($app) {
+return static function (App $app) {
 
   $authMiddleware = function (Request $request, RequestHandler $handler): Response {
     $jwt = $request->getHeaderLine('Authorization');
@@ -20,7 +21,7 @@ return static function ($app) {
     try {
       $key = new Key('7w8&^7af9*!o%j#)b$#k*p2w#q9@s1z&3n1!&y^vq36znm7!%h', 'HS256');
       $decoded = JWT::decode($jwt, $key);
-    } catch (\UnexpectedValueException $e) {
+    } catch (Exception) {
       throw new Exception('Forbidden: you are not authorized.', 403);
     }
 
