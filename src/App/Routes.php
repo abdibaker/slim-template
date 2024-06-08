fa080b4d7f8fe9310809a14335b9850c345aeb46
Abdillah Bakari <abdibaker1@gmail.com>
2024-06-08 13:45:25 +0300

    chore: Refactor authentication middleware to use JWT token directly

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
@@ -10,28 +11,22 @@ use Firebase\JWT\Key;
 return static function ($app) {
 
   $authMiddleware = function (Request $request, RequestHandler $handler): Response {
-    $jwtHeader = $request->getHeaderLine('Authorization');
-    if (!$jwtHeader) {
-      throw new Exception('JWT Token required.', 400);
-    }
+    $jwt = $request->getHeaderLine('Authorization');
 
-    $jwt = explode('Bearer ', $jwtHeader);
-    if (count($jwt) !== 2) {
-      throw new Exception('JWT Token invalid.', 400);
+    if (empty($jwt)) {
+      throw new Exception('JWT Token required.', 400);
     }
 
     try {
       $key = new Key('7w8&^7af9*!o%j#)b$#k*p2w#q9@s1z&3n1!&y^vq36znm7!%h', 'HS256');
-      $decoded = JWT::decode($jwt[1], $key);
+      $decoded = JWT::decode($jwt, $key);
     } catch (\UnexpectedValueException $e) {
       throw new Exception('Forbidden: you are not authorized.', 403);
     }
 
-    $object = $request->getParsedBody() ?: [];
-    $object['decoded'] = $decoded;
-
-    // You can also modify the request with $request = $request->withParsedBody($object);
-    $request = $request->withParsedBody($object);
+    $parsedBody = $request->getParsedBody() ?: [];
+    $parsedBody['decoded'] = $decoded;
+    $request = $request->withParsedBody($parsedBody);
 
     return $handler->handle($request);
   };
